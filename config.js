349014625d7a6961d2cd445963dea857b984eda8
Ganesh Rai <gnsh_sh_rai@yahoo.co.in>
2019-07-15 07:45:12 +0100

    pirov2 1.0.0

--- config.js
+++ config.js
@@ -1,21 +1,4 @@
-//Put all your configurations here
-
 module.exports= {
-    //walky configurations
-    walkyConf:{
-        motor1DirectionOnePin: 5,
-        motor1DirectionTwoPin: 6,
-        motor2DirectionOnePin: 20,
-        motor2DirectionTwoPin: 21,
-        ultraSonicTrigger: 16,
-        ultraSonicReciver: 24,
-        goFwdEvent: "goFwd", // Client triggered event to go Forward
-        goBkdEvent: "goBkd", // Client triggered event to go Backward
-        goLftEvent: "goLft", // Client triggered event to go Left
-        goRitEvent: "goRit", // Client triggered event to go Right
-        stopEvent: "stop", // Client triggered event to Stop
-        servingEvent: "serving", // Server triggered event to let client know of socketID controlling walky
-        blockDetectEvent: "blockdetected" // Server triggered event to send the front block distance detected 
-
-    }
+    //Put all your configurations here, Check plugins page on git or npm for samples
+    
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var configuration = require('./config')
 
 const iosock = function (io) {
-    // Put all your socketio routes here
+    // Put all your socketio routes here, check plugins page on git or npm to see samples
 }
 module.exports = iosock
